ef74c40a3c301eaee9b22fcbc39aaf74a82fefee
Matt Camack <mcamack@gmail.com>
2017-11-17 22:32:37 -0800

    test

--- ZEC.py
+++ ZEC.py
@@ -1,9 +1,7 @@
 import sys, pprint, requests
 
-coin = sys.argv[1]
-# pp = pprint.PrettyPrinter(indent=4)
-
 def getRate(coin):
     return requests.get('https://api.coinmarketcap.com/v1/ticker/%s/?convert=USD' %coin).json()
 
 print(getRate('bitcoin')[0]['price_usd'])
+print(getRate('zcash')[0]['price_usd'])
